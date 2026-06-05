a254491758d216ba8faaa63aaef153d404781743
wrld1 <kir260302@gmail.com>
2026-06-05 07:56:24 +0300

    Adjust Dockerfiles

--- microservices-backend/apps/notifications/src/notifications.service.ts
+++ microservices-backend/apps/notifications/src/notifications.service.ts
@@ -3,15 +3,15 @@ import { ProductCreatedEvent, ProductDeletedEvent } from '@app/common';
 
 @Injectable()
 export class NotificationsService {
-  private readonly logger = new Logger(NotificationsService.name);
+  private readonly eventLogger = new Logger('Events');
 
   handleProductCreated(data: ProductCreatedEvent) {
-    this.logger.log(
+    this.eventLogger.log(
       `Product created: "${data.name}" (id: ${data.id}, price: ${data.price})`,
     );
   }
 
   handleProductDeleted(data: ProductDeletedEvent) {
-    this.logger.log(`Product deleted: "${data.name}" (id: ${data.id})`);
+    this.eventLogger.log(`Product deleted: "${data.name}" (id: ${data.id})`);
   }
 }
